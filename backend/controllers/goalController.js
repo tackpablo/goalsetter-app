4fa167849993a4b5814acecd381de7dbfeef8e51
Pablo <tackpablo@gmail.com>
2022-03-27 17:48:29 -0700

    add deleting of user goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -74,6 +74,20 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
+  const user = await User.findById(req.user.id);
+
+  // check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
   await Goal.remove();
 
   res.status(200).json({ id: req.params.id });
