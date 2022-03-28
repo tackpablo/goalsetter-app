010389cd181e039e326e19c18ae6e6e06647de01
Pablo <tackpablo@gmail.com>
2022-03-27 18:40:54 -0700

    add react router

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">GoalSetter</Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
