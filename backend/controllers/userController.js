c210fc519e7d5a765ed7d10054f552f6b98c54ea
Pablo <tackpablo@gmail.com>
2022-03-27 17:17:30 -0700

    add jwt generation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -59,6 +60,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -74,5 +76,16 @@ const getMe = asyncHandler(async (req, res) => {
     message: "User data display",
   });
 });
+
+// generate JWT
+const generateToken = (id) => {
+  return jwt.sign(
+    {
+      id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "30d" }
+  );
+};
 
 module.exports = { registerUser, loginUser, getMe };
